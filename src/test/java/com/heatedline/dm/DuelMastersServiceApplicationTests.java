971726b62df5e23c03c1308d0ae936a773884402
supratim <supratim.c@finlabsindia.com>
2020-04-25 10:31:25 +0530

    first commit

---
+++ src/test/java/com/heatedline/dm/DuelMastersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatedline.dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuelMastersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
